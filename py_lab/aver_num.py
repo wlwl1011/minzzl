0c661c1f9b37612836d0a5037ff747c7f28a99d9
wlwl1011 <minji001011@naver.com>
2020-04-09 02:38:42 +0900

    first question

---
+++ py_lab/aver_num.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+input1 = int(input("Please input the number"))
+mysum=0
+element=0
+for i in range(0,input1):
+	element=int(input())
+	mysum+=element
+print(mysum/input1)
+
+
+
